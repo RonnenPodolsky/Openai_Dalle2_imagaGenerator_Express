e4c9c92de0ebfbe1ebb9b8b154741d16404c1a17
ronnenn podolsky <podolsky.ronnen@gmail.com>
2022-12-24 23:43:38 +0200

    working

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # openai
+
+using DALLE 2 from openAi to fetch images. Node-Express server with basic static frontend.
--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
-const {router} = require('./routes/openaiRoutes.js')
+const { router } = require('./routes/openaiRoutes.js')
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/openai', router)
 
