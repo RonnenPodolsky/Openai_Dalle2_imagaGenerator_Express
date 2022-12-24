71832b21f743fbab723334cfe264de5078e279a6
ronnenn podolsky <podolsky.ronnen@gmail.com>
2022-12-24 22:47:16 +0200

    exress skeleton

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(123)
+import express from 'express'
+import * as dotenv from 'dotenv'
+import {openaiRouter} from './routes/openaiRoutes.js'
+dotenv.config()
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use('/openai', openaiRouter)
+
+app.listen(port, () => console.log(`running on port ${port}`))
