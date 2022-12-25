f5d6a6fb1274b27a8e64881ae4d36b01f4c36b70
Ronnen Podolsky <95850169+RonnenPodolsky@users.noreply.github.com>
2022-12-25 17:09:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors');
+
 const dotenv = require('dotenv').config()
 const { router } = require('./routes/openaiRoutes.js')
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
